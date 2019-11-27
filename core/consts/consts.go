98fbd2646064546d8efd6f9122757f64f91c7c5e
Tree <tree@ejyi.com>
2019-11-27 20:46:33 +0800

    添加文件

---
+++ core/consts/consts.go
@@ -0,0 +1,11 @@
+package consts
+
+//默认空时间
+const BlankTime = "1970-01-01 00:00:00"
+const BlankDate = "1970-01-01"
+
+//全局日期格式
+const AppDateFormat = "2006-01-02"
+const AppTimeFormat = "2006-01-02 15:04:05"
+const AppSystemTimeFormat = "2006-01-02T15:04:05Z"
+const AppSystemTimeFormat8 = "2006-01-02T15:04:05+08:00"
