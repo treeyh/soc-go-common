ec2444c8dc5ebdd7d7062cbb8e3dfe79217a6de9
tree <tree@ejyi.com>
2020-07-13 21:15:22 +0800

    调整network方法

--- core/utils/network/network.go
+++ core/utils/network/network.go
@@ -32,9 +32,9 @@ func GetIntranetIp() string {
 
 	for _, address := range addrs {
 		// 检查ip地址判断是否回环地址
-		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				_localIp = ipnet.IP.String()
+		if ipNet, ok := address.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil {
+				_localIp = ipNet.IP.String()
 				break
 			}
 		}
