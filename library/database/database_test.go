cf4c4406cd2637a606af8f4370274fca19d64327
hai.yu <hai.yu@snowballtech.com>
2021-04-12 19:50:41 +0800

    调整common代码

--- library/database/database_test.go
+++ library/database/database_test.go
@@ -2,35 +2,50 @@ package database
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"github.com/treeyh/soc-go-common/core/config"
-	"github.com/treeyh/soc-go-common/core/errors"
-	"github.com/treeyh/soc-go-common/core/logger"
 	"github.com/treeyh/soc-go-common/core/utils/file"
 	"github.com/treeyh/soc-go-common/core/utils/json"
 	"path"
+	"strconv"
 	"testing"
 	"time"
 )
 
-type ObjectIdPo struct {
+/**
+
+CREATE TABLE `test_student` (
+`id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+`name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '姓名',
+`code` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '学号',
+`birthday` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '生日',
+`height` DECIMAL(5,2) NOT NULL DEFAULT '0' COMMENT '身高',
+`creator` bigint NOT NULL DEFAULT '0' COMMENT '创建人',
+`create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+`updator` bigint NOT NULL DEFAULT '0' COMMENT '更新人',
+`update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+`del_flag` tinyint NOT NULL DEFAULT '2' COMMENT '是否删除,1是,2否',
+PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC COMMENT='学生';
+
+*/
+
+type StudentPo struct {
 
 	// Id 主键
 	Id int64 `gorm:"type:bigint(20);column:id" json:"id"`
 
-	// OrgId 组织id
-	OrgId int64 `gorm:"type:bigint(20);column:org_id" json:"orgId"`
+	// Name 姓名
+	Name string `gorm:"type:varchar(32);column:name" json:"name"`
 
-	// SysCode 系统编号
-	SysCode string `gorm:"type:varchar(32);column:sys_code" json:"sysCode"`
-
-	// Code 对象编号
+	// Code 学号
 	Code string `gorm:"type:varchar(64);column:code" json:"code"`
 
-	// MaxId 当前最大编号
-	MaxId int64 `gorm:"type:bigint(20);column:max_id" json:"maxId"`
+	// Birthday 生日
+	Birthday time.Time `gorm:"type:datetime;column:birthday" json:"birthday"`
 
-	// Step 步长
-	Step int `gorm:"type:int(11);column:step" json:"step"`
+	// Height 身高
+	Height float64 `gorm:"type:int(11);column:height" json:"height"`
 
 	// Creator 创建人
 	Creator int64 `gorm:"type:bigint(20);column:creator" json:"creator"`
@@ -44,15 +59,12 @@ type ObjectIdPo struct {
 	// UpdateTime 更新时间
 	UpdateTime time.Time `gorm:"type:datetime;column:update_time" json:"updateTime"`
 
-	// Version 乐观锁
-	Version int `gorm:"type:int(11);column:version" json:"version"`
-
 	// DelFlag 是否删除,1是,2否
 	DelFlag int `gorm:"type:tinyint(4);column:del_flag" json:"delFlag"`
 }
 
-func (*ObjectIdPo) TableName() string {
-	return "sys_object_id"
+func (*StudentPo) TableName() string {
+	return "test_student"
 }
 
 func initDb() {
@@ -73,37 +85,36 @@ func initDb() {
 func TestGetDB(t *testing.T) {
 	initDb()
 
-	objectId := ObjectIdPo{
-		Id:         1,
-		OrgId:      1,
-		SysCode:    "syscode",
-		Code:       "user",
-		MaxId:      1000,
-		Step:       200,
+	now := time.Now().Unix()
+
+	studentPo := StudentPo{
+		Id:         now,
+		Name:       "name_" + strconv.FormatInt(now, 10),
+		Code:       "code_" + strconv.FormatInt(now, 10),
+		Birthday:   time.Now(),
+		Height:     1.81,
 		Creator:    23,
 		CreateTime: time.Now(),
 		Updator:    45,
 		UpdateTime: time.Now(),
-		Version:    1,
 		DelFlag:    2,
 	}
 
-	err := GetDb().Create(&objectId).Error
-	//t := GetDb().NewRecord(&objectId)
-	//fmt.Println(t)
-	fmt.Println(err)
-	logger.Logger().Info(errors.NewAppErrorByExistError(errors.DbOperationFail, err))
+	err := GetDb().Create(&studentPo).Error
 
-	fmt.Println(json.ToJsonIgnoreError(objectId))
-}
+	assert.NoError(t, err)
 
-func TestGetDB2(t *testing.T) {
-	initDb()
+	fmt.Println(json.ToJsonIgnoreError(studentPo))
 
-	var objId ObjectIdPo
+	var studentPo2 StudentPo
+	row := GetDb().Where("id = ?", now).Where("del_flag = ?", 2).First(&studentPo2).RowsAffected
 
-	row := GetDb().Where("org_id = ? AND code = ?", 1, "us1er").Where("del_flag = ?", 2).First(&objId).RowsAffected
+	assert.Equal(t, row, int64(1))
+	assert.Equal(t, studentPo2.Id, now)
 
-	fmt.Println(row)
-	fmt.Println(json.ToJsonIgnoreError(objId))
+	var studentPo3 StudentPo
+	row2 := GetDb().Where("id = ?", 1).Where("del_flag = ?", 2).First(&studentPo3).RowsAffected
+
+	assert.NotEqual(t, row2, int64(1))
+	assert.Equal(t, studentPo3.Id, int64(0))
 }
