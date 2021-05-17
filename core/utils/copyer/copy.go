52371457be710017b66b7cc4af5bacc31fc49954
tree <tree@ejyi.com>
2021-05-17 21:02:35 +0800

    调整common代码

--- core/utils/copyer/copy.go
+++ core/utils/copyer/copy.go
@@ -1,20 +1,42 @@
 package copyer
 
 import (
+	"context"
 	"github.com/treeyh/soc-go-common/core/errors"
 	"github.com/treeyh/soc-go-common/core/logger"
 	"github.com/treeyh/soc-go-common/core/utils/json"
 )
 
-func Copy(src interface{}, source interface{}) errors.AppError {
+func Copy(ctx context.Context, src interface{}, target interface{}) errors.AppError {
+	if src == nil {
+		return nil
+	}
+
 	jsonStr, err := json.ToJson(src)
 	if err != nil {
-		logger.Logger().Error(err)
+		logger.Logger().ErrorCtx(ctx, err)
 		return errors.NewAppError(errors.ObjectCopyFail)
 	}
-	err = json.FromJson(jsonStr, source)
+	err = json.FromJson(jsonStr, target)
 	if err != nil {
-		logger.Logger().Error(err)
+		logger.Logger().ErrorCtx(ctx, err)
+		return errors.NewAppError(errors.ObjectCopyFail)
+	}
+	return nil
+}
+
+func CopyList(ctx context.Context, src []interface{}, target []interface{}) errors.AppError {
+	if len(src) <= 0 {
+		return nil
+	}
+	jsonStr, err := json.ToJson(src)
+	if err != nil {
+		logger.Logger().ErrorCtx(ctx, err)
+		return errors.NewAppError(errors.ObjectCopyFail)
+	}
+	err = json.FromJson(jsonStr, target)
+	if err != nil {
+		logger.Logger().ErrorCtx(ctx, err)
 		return errors.NewAppError(errors.ObjectCopyFail)
 	}
 	return nil
