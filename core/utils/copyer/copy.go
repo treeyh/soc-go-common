f8aa34f7e721d7e2404278670855636824975ae4
tree <tree@ejyi.com>
2021-05-20 15:31:21 +0800

    调整common代码

--- core/utils/copyer/copy.go
+++ core/utils/copyer/copy.go
@@ -5,6 +5,7 @@ import (
 	"github.com/treeyh/soc-go-common/core/errors"
 	"github.com/treeyh/soc-go-common/core/logger"
 	"github.com/treeyh/soc-go-common/core/utils/json"
+	"reflect"
 )
 
 func Copy(ctx context.Context, src interface{}, target interface{}) errors.AppError {
@@ -25,15 +26,18 @@ func Copy(ctx context.Context, src interface{}, target interface{}) errors.AppEr
 	return nil
 }
 
-func CopyList(ctx context.Context, src []interface{}, target []interface{}) errors.AppError {
-	if len(src) <= 0 {
-		return nil
+func CopyList(ctx context.Context, src interface{}, target interface{}) errors.AppError {
+	if reflect.TypeOf(src).Kind() == reflect.Slice || reflect.TypeOf(src).Kind() == reflect.Array {
+		if reflect.ValueOf(src).Len() <= 0 {
+			return nil
+		}
 	}
 	jsonStr, err := json.ToJson(src)
 	if err != nil {
 		logger.Logger().ErrorCtx(ctx, err)
 		return errors.NewAppError(errors.ObjectCopyFail)
 	}
+	print(jsonStr)
 	err = json.FromJson(jsonStr, target)
 	if err != nil {
 		logger.Logger().ErrorCtx(ctx, err)
--- core/utils/copyer/copy_test.go
+++ core/utils/copyer/copy_test.go
@@ -1,16 +1,17 @@
 package copyer
 
 import (
+	"context"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 type Source struct {
-	Name string
+	Name string `json:"name"`
 }
 
 type Destination struct {
-	Name string
+	Name string `json:"name"`
 }
 
 func TestCopy(t *testing.T) {
@@ -22,7 +23,23 @@ func TestCopy(t *testing.T) {
 
 	ss := &[]*Source{s}
 	dd := &[]*Destination{d}
-	Copy(ss, dd)
+	Copy(context.Background(), ss, dd)
 
-	assert.Equal(t,"test", (*dd)[0].Name )
+	assert.Equal(t, "test", (*dd)[0].Name)
+
+	ls := make([]Source, 0)
+	ls = append(ls, Source{
+		Name: "test",
+	}, Source{
+		Name: "test2",
+	})
+
+	ds := make([]Destination, len(ls))
+
+	err := CopyList(context.Background(), ls, &ds)
+	assert.NoError(t, err)
+	assert.True(t, len(ds) > 0)
+
+	t.Log(ds)
+
 }
