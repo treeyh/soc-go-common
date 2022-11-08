67ffd53c92141c7b9af815a46ef9c0e626ea1e93
tree <tree@ejyi.com>
2022-11-08 19:19:36 +0800

    调整日志策略

--- library/wechat/sns_wechat_test.go
+++ library/wechat/sns_wechat_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/treeyh/soc-go-common/core/config"
 	"github.com/treeyh/soc-go-common/core/utils/file"
 	"github.com/treeyh/soc-go-common/core/utils/json"
+	"github.com/treeyh/soc-go-common/tests"
 	"path"
 	"testing"
 )
@@ -30,7 +31,38 @@ func TestDecrypt(t *testing.T) {
 
 	initWechatTestConfig()
 
-	resp, err := GetProxy().JsCode2Session(nil, "081GD5tq0egYKl1FfIrq0deGsq0GD5tm")
+	resp, err := GetProxy().JsCode2Session(nil, "0519YF000PHPRO1QkK100CAynK29YF0V")
+	if err != nil {
+		assert.NoError(t, err)
+		return
+	}
+
+	assert.True(t, CheckErrCodeSucceed(200, resp.ErrCode))
+
+	// {\"errcode\":0,\"errmsg\":\"\",\"openid\":\"oO7wo5EvObtcivnQtHxs8kmbn30A\",\"session_key\":\"WdU2sNq8VFlvXgAgFcUkSg==\",\"unionid\":\"ot07f0ZtRZ2A_sxv_jtYhpcbhqec\"}
+	log.Info(json.ToJsonIgnoreError(resp))
+
+	//result, err := GetProxy().DecryptEncryptedData(nil, resp.SessionKey,
+	//	"HydtA2GK6zoPE7K4JN0HWU/uHI3sMkZ4F0dexYrg5pfBfANh42…Q1r9KxXUE8DqtIPEKRdvDShiZcbRF/vXxkRXswrjsWOmHFR0=",
+	//	"SmblgkW2b5fDmZhXSxiukg==", true)
+
+	result, err2 := GetProxy().DecryptEncryptedData(nil, resp.SessionKey,
+		"IRNdYNi84ZEk/yZwBLsnrHcH4M/2FD5KMXphZZpza9zlokR87hnQHoCGksovkZC+KGPAe9HRtSTg2ifWJ31jHxnsSn6XJYmWiZ7+de2MINVIycVDgqTG6niJDp4OYPiM1lpsr5JZUjcDmH1tTC62rZLlknGwzeCZZ9wD1Jp32W1bovLFvlbjNwYMWpk5OBSP2vnzC9yqPgvGLuxhYTEhxiz6BiZPAPaj7+7XBzv+gjrmiS7yrnAZIUkSHnvwVA0zzSW1lEr/NXkZx60g9JYFSP38ViIDXLvv8UzEHBZ5FXNFsbsI1fKk7ePdk9HMom/Ewv+7BV44MjR+K3EEPQMDsYu/b9WGZ0w6pHZh8upRE6kEQBVhKOhmYhZaClxgG7rTLZ+cSyIpaFQEar/SQavinh4+4nCzprJn0fM4YGwkhE4=",
+		"dLwV9296bU7nPGkzldrrjQ==", true)
+
+	log.Info(err2)
+	log.Info(result)
+	log.Info(json.ToJsonIgnoreError(result))
+
+}
+
+func TestAppLogin(t *testing.T) {
+
+	ctx := tests.GetNewContext()
+
+	initWechatTestConfig()
+
+	resp, err := GetProxy().GetAccessTokenByCode(ctx, "0115N6000zeoSO1afT3000iPhm45N60o")
 	if err != nil {
 		assert.NoError(t, err)
 		return
@@ -40,13 +72,7 @@ func TestDecrypt(t *testing.T) {
 
 	log.Info(json.ToJsonIgnoreError(resp))
 
-	//result, err := GetProxy().DecryptEncryptedData(nil, resp.SessionKey,
-	//	"HydtA2GK6zoPE7K4JN0HWU/uHI3sMkZ4F0dexYrg5pfBfANh42…Q1r9KxXUE8DqtIPEKRdvDShiZcbRF/vXxkRXswrjsWOmHFR0=",
-	//	"SmblgkW2b5fDmZhXSxiukg==", true)
-
-	result, err2 := GetProxy().DecryptEncryptedData(nil, resp.SessionKey,
-		"aTossU7l+VWo7Eaz2+RsW3KecI0OSu4fZ/YyQwlb/SyOE8CK2ifbBXD7qb5GlG790OUbseKmZgF9e/AkXsMfaYY7D0bVjX9Bngh0z8CL+0eQFD7/Tjv6EoYLkk8LQogAiEUFlOMyv//8Xl8iDYVlxyYuYsSJQKNWrLdcJUhSruk14lig5pZdfD76rf12pdXmk66w5ERAP/7LIdTkOyEU3fJGVLhFteUvGdsVwwg1Q1Mczp6mzNSiDP2O6c+8X9lxpPVPk4rx+8gEYUxhZH97Wn1SxiBnfuHZMBPH5JVWZ5QGkO9SNa+AwBlnK5GM/m48SKx28YGDhTBcGqrQAMl0JdHhNqBOAROm09FURHV4eccnkFRMKPV6DbElDsW6cI5QIX9g/lsgnW3Vb4MhLC8RFCqQX6PVIQKDAWNsflIbDYa7ku5uUOQnqf4hYjtD54bJ10YM+c4kOwhPxllMkArnUwehPTknnAw+vtG2bQ37+GE=",
-		"1S60BLqmpZuzljJG2/sPIA==", true)
+	result, err2 := GetProxy().GetSnsUserInfo(nil, resp.AccessToken, resp.Openid, "zh_CN")
 
 	log.Info(err2)
 	log.Info(result)
